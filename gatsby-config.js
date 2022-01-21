ae2c69ea68a434b19b367c677522137b93b57046
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2022-01-21 16:47:43 -0300

    added tailwind support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,30 @@
 module.exports = {
   siteMetadata: {
-      title: `portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "3204305538"
-    }
-  }, {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    `gatsby-plugin-postcss`,
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "3204305538",
+      },
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
