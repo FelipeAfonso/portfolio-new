6575894999f6da55faa394742139af0736393669
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2022-03-12 13:57:32 -0300

    chore: upgraded file system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import { Contact } from "./components/Contact";
-import { Header } from "./components/Header";
+import { Contact } from "./sections/Contact";
+import { Header } from "./sections/Header";
 import { ProjectPage } from "./components/ProjectPage";
-import { Projects } from "./components/Projects";
+import { Projects } from "./sections/Projects";
 
 function App() {
   return (
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,5 +1,5 @@
+import { ProjectCard } from "../components/ProjectCard";
 import { projects } from "../content/projects";
-import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
   return (
