cf7ca2064b1455c474fa57aaf049769a066c5fce
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2022-03-13 10:59:03 -0300

    feat: added hover animation to project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { Project } from "../types";
@@ -7,25 +8,31 @@ export const ProjectCard: FC<{ project: Project; projectKey: string }> = ({
   projectKey,
 }) => {
   return (
-    <Link
-      to={`/project/${projectKey}`}
-      className="bg-white bg-opacity-10 min-w-full sm:min-w-min sm:w-96 p-2 rounded cursor-pointer"
-      reloadDocument
+    <motion.div
+      whileHover={{
+        scale: 1.07,
+        backgroundColor: "#ffffffaa",
+      }}
+      className="bg-white bg-opacity-5 min-w-full sm:min-w-min sm:w-96 p-2 rounded cursor-pointer"
     >
-      <p className="text-2xl text-gray-50 font-bold">{project.title}</p>
-      {project.images?.[0] ? (
-        <img
-          src={project.images[0]}
-          className="min-w-full max-h-48 object-contain bg-black mt-2"
-        />
-      ) : (
-        <div className="mt-2">
-          <text className="text-base text-gray-50">{`${project.description.slice(
-            0,
-            330
-          )}${project.description.length > 330 ? "..." : ""}`}</text>
-        </div>
-      )}
-    </Link>
+      <Link to={`/project/${projectKey}`} reloadDocument>
+        <p className="text-2xl text-gray-50 font-bold">{project.title}</p>
+        {project.images?.[0] ? (
+          <motion.img
+            animate={{ filter: "blur(0.5px)" }}
+            whileHover={{ filter: "blur(0px)" }}
+            src={project.images[0]}
+            className="min-w-full max-h-48 object-contain bg-black mt-2"
+          />
+        ) : (
+          <div className="mt-2">
+            <text className="text-base text-gray-50">{`${project.description.slice(
+              0,
+              330
+            )}${project.description.length > 330 ? "..." : ""}`}</text>
+          </div>
+        )}
+      </Link>
+    </motion.div>
   );
 };
